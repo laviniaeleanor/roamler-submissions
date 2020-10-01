955ad8e54f7cf5e498f7ce9791f8aea12e60cc6e
laviniaeleanor <laviniaeleanor@outlook.com>
2020-10-01 11:09:38 +0200

    client: add material ui and AppBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import './App.css';
+import AppBar from './components/AppBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={'https://www.roamler.com/assets/logo-roamler-main-15102f48afec6394db0c63386b0d72d9.svg'} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar />
     </div>
   );
 }
---
+++ client/src/components/AppBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+export default function ButtonAppBar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <img 
+          alt="Roamler logo" 
+          style={{height: 38}}
+          src="https://www.roamler.com/assets/logo-roamler-main-15102f48afec6394db0c63386b0d72d9.svg" 
+        />
+      </Toolbar>
+    </AppBar>
+  );
+}
