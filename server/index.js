b30a5bcaba669c055ede9b190020da43ff5ee308
laviniaeleanor <laviniaeleanor@outlook.com>
2020-10-01 13:54:26 +0200

    server: add CORS

--- server/index.js
+++ server/index.js
@@ -12,6 +12,21 @@ const eraseDatabaseOnSync = false;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+
 app.get('/submissions', (req, res) => {
   const {query} = req;
   getSubmissions(query)
